0034492ec87042ad1008d56d0da5cc48a6d59356
dbonach <deivitlopes@gmail.com>
2021-05-16 01:14:29 -0300

    Add all functionality

--- script.js
+++ script.js
@@ -9,6 +9,56 @@ const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
 
+let itemCount = 1;
+let uncheckedCount = 1;
+
 function newTodo() {
-  alert('New TODO button clicked!')
+  let new_todo = prompt("Enter a new todo: ");
+  let new_item = document.createElement("li");
+  new_item.className = classNames.TODO_TEXT;
+
+  let input = document.createElement("input");
+  input.type = "checkbox"
+  input.setAttribute("onclick", "check(event.target)");
+  console.log(input);
+  new_item.appendChild(input);
+
+  let label = document.createElement("label");
+  label.innerHTML = new_todo;
+  new_item.appendChild(label);
+
+  let button = document.createElement("button");
+  button.innerHTML = "x";
+  button.setAttribute("class", "delete");
+  button.setAttribute("onclick", "removeItem(event)");
+  new_item.appendChild(button);
+  console.log(new_item)
+
+  list.appendChild(new_item);
+
+
+  itemCount += 1;
+  uncheckedCount += 1;
+  itemCountSpan.innerHTML = itemCount;
+  uncheckedCountSpan.innerHTML = uncheckedCount;
+}
+
+function check(target) {
+
+  if (target.checked) {
+    uncheckedCount -= 1;
+  } else {
+    uncheckedCount += 1;
+  }
+  uncheckedCountSpan.innerHTML = uncheckedCount;
+}
+
+function removeItem(target) {
+
+  target.path[1].remove();
+  itemCount -= 1;
+  uncheckedCount -= 1;
+  itemCountSpan.innerHTML = itemCount;
+  uncheckedCountSpan.innerHTML = uncheckedCount;
+
 }
